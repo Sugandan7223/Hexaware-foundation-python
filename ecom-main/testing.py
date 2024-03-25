94d7aa24111ac91dbf2b0762552983e48083522c
Sugandan Elangovan <110321519+Sugandan7223@users.noreply.github.com>
2024-03-25 15:06:56 +0530

    Update testing.py

--- ecom-main/testing.py
+++ ecom-main/testing.py
@@ -3,21 +3,21 @@ import ecom
 
 class TestEcommerceSystem(unittest.TestCase):
     def setUp(self):
-        # Set up the environment for testing
+        
         self.order_processor = ecom.entity.OrderProcessorRepositoryImpl()
 
     def tearDown(self):
-        # Clean up after testing
+        
         pass
 
     def test_create_product_success(self):
-        # Test whether product creation is successful
+       
         product = {'product_id':81,'name': 'Test Product', 'description': 'Test Description', 'price': 50.0, 'stock_quantity': 10}
         result = self.order_processor.createProduct(product)
         self.assertTrue(result)
 
     def test_add_to_cart_success(self):
-        # Test whether product is added to the cart successfully
+      
         cart_id = 902
 
         customer_id = 1
@@ -29,21 +29,21 @@ class TestEcommerceSystem(unittest.TestCase):
         self.assertTrue(result)
 
     def test_place_order_success(self):
-        # Test whether order placement is successful
+      
         order_id=901
-        customer = {'customer_id': 1, 'cart_id': 1}  # Assuming 'cart_id' is required for placing an order
+        customer = {'customer_id': 1, 'cart_id': 1} 
         products_quantity_map = self.order_processor.retrieveProductsFromCart(customer['customer_id'],customer['cart_id']) # Example: {product_id: quantity}
         shipping_address = 'Test Address'
         result = self.order_processor.placeOrder(order_id,customer, products_quantity_map, shipping_address)
         self.assertTrue(result)
 
     def test_customer_not_found_exception(self):
-        # Test whether CustomerNotFoundException is raised when customer id is not found
+       
         with self.assertRaises(ecom.exception.CustomerNotFoundException):
             self.order_processor.getOrdersByCustomer(999)
 
     def test_product_not_found_exception(self):
-        # Test whether ProductNotFoundException is raised when product id is not found
+      
         with self.assertRaises(ecom.exception.ProductNotFoundException):
             self.order_processor.deleteProductByID(999)
 
